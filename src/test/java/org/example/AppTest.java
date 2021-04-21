bab7080852c6547d0d2247b5adcf402b21c8f14d
ing.conti <ingconti@ingconti.com>
2021-04-21 19:57:31 +0200

    fixed

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -13,19 +13,6 @@ public class AppTest
         assertTrue( true );
     }
 
-    @Test
-    public void testGameBoard(){
-        GameBoard gb = new GameBoard();
-        boolean init_OK = gb.readConfig();
-        assertTrue(init_OK);
-    }
-
-    @Test
-    public void testGameBoardWithPlayersLogged(){
-        GameBoard gb = new GameBoard();
-        boolean init_OK = gb.readConfig();
-        assertTrue(init_OK);
-    }
 
 
 }
